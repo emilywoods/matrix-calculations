8f95d36dd534843d4922fdfb8d217e191336af15
Emily Woods <emilymonica.woods@gmail.com>
2017-08-27 17:49:04 +0100

    Complete parser

--- spec/matrix_parser_spec.rb
+++ spec/matrix_parser_spec.rb
@@ -6,30 +6,38 @@ RSpec.describe 'parser' do
   subject(:parser) {MatrixParser.new}
 
   it 'should display an error message when matrix value is not a number' do
-    source = ["2 1", "B 1"]
+    matrix = ["2 1", "B 1"]
 
-    expect{parser.parse(2, source)}.to raise_error(InvalidMatrixValue)
-    expect{parser.parse(2, source)}.to raise_error.with_message(InvalidMatrixValue::INVALID_MATRIX_VALUE)
+    expect{parser.parse(2, matrix)}.to raise_error(InvalidMatrixValue)
+    expect{parser.parse(2, matrix)}.to raise_error.with_message(InvalidMatrixValue::INVALID_MATRIX_VALUE)
   end
 
   it 'should display an error message when row is blank' do
-    source = [" "]
+    matrix = [" "]
 
-    expect{parser.parse(2, source)}.to raise_error(InvalidMatrixValue)
-    expect{parser.parse(2, source)}.to raise_error.with_message(InvalidMatrixValue::INVALID_MATRIX_VALUE)
+    expect{parser.parse(2, matrix)}.to raise_error(InvalidMatrixValue)
+    expect{parser.parse(2, matrix)}.to raise_error.with_message(InvalidMatrixValue::INVALID_MATRIX_VALUE)
   end
 
   it 'should display an error message when row is incomplete' do
-    source = ["2 1", "2"]
+    matrix = ["2 1", "2"]
 
-    expect{parser.parse(2, source)}.to raise_error(InvalidMatrixValue)
-    expect{parser.parse(2, source)}.to raise_error.with_message(InvalidMatrixValue::INVALID_MATRIX_VALUE)
+    expect{parser.parse(2, matrix)}.to raise_error(InvalidMatrixValue)
+    expect{parser.parse(2, matrix)}.to raise_error.with_message(InvalidMatrixValue::INVALID_MATRIX_VALUE)
   end
 
   it 'should display an error message when row has too many values' do
-    source = ["2 1", "2 1 2"]
+    matrix = ["2 1", "2 1 2"]
 
-    expect{parser.parse(2, source)}.to raise_error(InvalidMatrixValue)
-    expect{parser.parse(2, source)}.to raise_error.with_message(InvalidMatrixValue::INVALID_MATRIX_VALUE)
+    expect{parser.parse(2, matrix)}.to raise_error(InvalidMatrixValue)
+    expect{parser.parse(2, matrix)}.to raise_error.with_message(InvalidMatrixValue::INVALID_MATRIX_VALUE)
+  end
+
+  it 'should correctly parse a valid matrix' do
+    matrix = ["2 1", "2 4"]
+
+    result = parser.parse(2, matrix)
+
+    expect(result).to eq([[2, 1], [2, 4]])
   end
 end
